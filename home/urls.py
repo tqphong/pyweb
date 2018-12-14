1a67a16e0099fc857911c2047398352fa6273b40
tqphong <phongtran16395@gmail.com>
2018-12-14 12:48:08 +0700

    add abc

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-#app_name = 'home'
+#app_name = 'home' abc
 urlpatterns = [
     path('', views.index, ),
     path('contact/', views.contact, name='contact'),
